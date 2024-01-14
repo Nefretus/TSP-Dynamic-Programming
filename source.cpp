6bd16b99a448a1cc827ae2481ed757be447ff284
Nefretus <mat.mareK@onet.pl>
2024-01-14 12:10:14 +0100

    fixed

--- source.cpp
+++ source.cpp
@@ -1,27 +1,12 @@
 #include<iostream>
 #include<vector>
-#include<fstream>
-#include<cmath>
 
 class Tsp {
 public:
     Tsp(int n_nodes, unsigned int seed) {
-       // std::fstream file(filepath, std::ios_base::in);
-       // auto distance = [](const Coords& city, const Coords& other) -> float {
-       //     return static_cast<float>(std::sqrt(std::pow(city.x - other.x, 2) + std::pow(city.y - other.y, 2)));
-       // };
-       //
-       // int n_nodes;
-       // file >> n_nodes;
-       //
        cost_matrix = std::vector<std::vector<int>>(n_nodes, std::vector<int>(n_nodes, 0));
        memo = std::vector<std::vector<int>>(1 << n_nodes, std::vector<int>(n_nodes, 0));
        parent_matrix = std::vector<std::vector<int>>(1 << n_nodes, std::vector<int>(n_nodes, -1));
-       //
-       // std::vector<Coords> city(n_nodes, {0, 0});
-       // for (int i = 0; i < n_nodes; i++) 
-       //      file >> city[i].x >> city[i].y;
-       //
 
         for (int a = 0; a < n_nodes; a++) {
             for (int b = 0; b < n_nodes; b++) {
@@ -39,16 +24,18 @@ public:
     }
 
     int run(int city, int mask) {
-        if (mask == ((1 << cost_matrix.size()) - 1))
+        if (mask == ((1 << cost_matrix.size()) - 1)) {
             return cost_matrix[city][0];
+        }
 
-        if (memo[mask][city] != 0) 
+        if (memo[mask][city] != 0) {
             return memo[mask][city];
+        }
 
         int res = INT_MAX;
         for (int i = 0; i < cost_matrix.size(); i++) {
-            if ((mask & (1 << i)) == 0) {  
-                int current_res = cost_matrix[i][city] + run(i, mask | (1 << i));
+            if ((mask & (1 << i)) == 0) {
+                int current_res = cost_matrix[city][i] + run(i, mask | (1 << i));
                 if (current_res < res) {
                     res = current_res;
                     parent_matrix[mask][city] = i;
@@ -68,21 +55,20 @@ public:
         } while (cur_node != -1);
     }
 
-    std::vector<std::vector<int>> cost_matrix;
 private:
     struct Coords {
         int x, y;
     };
+    std::vector<std::vector<int>> cost_matrix;
     std::vector<std::vector<int>> memo;
     std::vector<std::vector<int>> parent_matrix;
 };
 
 int main() {
-    Tsp tsp(10, 10);
+    Tsp tsp(15, 15);
     std::cout << "------" << std::endl;
-    int res = tsp.run(0, 1);
+    std::cout << "Distance: " << tsp.run(0, 1) << std::endl;
+    std::cout << "Path: " << std::endl;
     tsp.print_route();
-    std::cout << res << std::endl;
 	return 0;
 }
-
